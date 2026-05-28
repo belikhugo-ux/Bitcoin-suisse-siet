5076855899b44569ac0ff1aba16e27903b263a13
belikhugo-ux <belikhugo@gmail.com>
2026-05-28 15:32:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,30 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
 
-app.use(express.static("public"));
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
 
+  console.log("User connected");
+
   socket.on("join", (roomId) => {
     socket.join(roomId);
+    console.log(`Joined room: ${roomId}`);
   });
 
   socket.on("signal", ({ roomId, data }) => {
     socket.to(roomId).emit("signal", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 
 });
@@ -23,5 +35,5 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`Server running on port ${PORT}`);
 });
